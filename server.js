682051a867675c1163be80bd1c191de7392ad4e5
Angel Torres <alas4224@gmail.com>
2019-03-12 15:36:22 -0500

    add cors again

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const helmet = require('helmet');
+const cors = require('cors');
 
 const server = express();
 const authRoute = require('./auth/authentication');
 const listsRoute = require('./lists/lists')
+
+server.use(cors());
 server.use(helmet());
 server.use(express.json());
 
