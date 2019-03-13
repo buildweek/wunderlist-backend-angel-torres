140250bd0e9a3e00a1beac53bd88706ed8bce2a6
Edwin Torres <edwin.torres.jr2@gmail.com>
2019-03-13 18:27:00 -0400

    defined cors just to be sure

--- lists/lists.js
+++ lists/lists.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const route = express.Router();
 const jwt = require("jsonwebtoken");
+const cors = require("cors");
 
 const db = require("../data/knexConfig");
 
